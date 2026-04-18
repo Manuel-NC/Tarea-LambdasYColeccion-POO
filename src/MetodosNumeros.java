7f5638a56bd97671783079613d426c0d63509c0a
Manuel <manu40.navarro@gmail.com>
2026-04-17 19:52:13 -0700

    Se agrego en MetodoCadenas el punto 5 (Mapa de Longitudes) con un metodo llamada mapaLongitudes, el cual recibe un ArrayList<String>, se le hace .stream(), se guarda la cadena y el valor de la cadena en un HashMap<String,Integer> y finalmente lo devuelve. Se hicieron pruebas en el main. Tambien se cambiaron los nombres de los metodos un poco y los sout del main.

--- src/MetodosNumeros.java
+++ src/MetodosNumeros.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class MetodosNumeros {
 
-    public static ArrayList<Integer> metodoMultiplicador(ArrayList<Integer> numeros, int factor) {
+    public static ArrayList<Integer> multiplicador(ArrayList<Integer> numeros, int factor) {
         ArrayList<Integer> numerosMultiplicados = new ArrayList<>(numeros);
 
         numerosMultiplicados.replaceAll(n -> n * factor);
@@ -13,7 +13,7 @@ public class MetodosNumeros {
         return numerosMultiplicados;
     }
 
-    public static HashSet<Integer> metodoCuadradosUnicos(ArrayList<Integer> numeros){
+    public static HashSet<Integer> cuadradosUnicos(ArrayList<Integer> numeros){
 
         HashSet<Integer> numerosUnicos = (HashSet<Integer>) numeros.stream()
                 .filter(n -> n%2 == 0)
